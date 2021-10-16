48d3dc9b9fbb5edc386094d618ab9889f0c4f149
Rafa Sánchez Lucena <sanchez.lucena@gmail.com>
2021-10-16 19:38:31 +0200

    Se modifican los comentarios

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-// Ejercicio de ordenacion de array sin utilizar métidos nativos.
+// Ejercicio de lógica de programación.
+// Ordena un array sin utilizar métodos nativos.
 
 const array = [8, 15, 23, 52, 35, 38, 40, 64, 18, 28];
 
@@ -7,7 +8,7 @@ console.log(`Array sin ordenar: ${array}`);
 for (let i = 0; i < array.length - 1; i++) {
     for (let j = 0; j < array.length - 1; j++) {
         if (array[j] < array[j + 1]) {
-            valorTemporal = array[j];  
+            valorTemporal = array[j];
             array[j] = array[j + 1];
             array[j + 1] = valorTemporal;
         };
